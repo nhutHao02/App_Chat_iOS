910df2b77fdea98b554dc91dca7feb424b5d2c94
nhutHao02 <trannhuthao091@gmail.com>
2024-02-25 14:00:27 +0700

    Initial Commit

---
+++ AppChat/AppChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppChatApp.swift
+//  AppChat
+//
+//  Created by TranHao on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppChat
+//
+//  Created by TranHao on 25/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
